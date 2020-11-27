74a01b5a16a1079837ab2ca7f31fe31ac2c8f618
Adrian Leung <39450742+AdrianLearn@users.noreply.github.com>
2020-11-27 14:10:58 -0800

    Update MenuScreen.js

--- app/screens/MenuScreen.js
+++ app/screens/MenuScreen.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import {Text, View, Image, ImageBackground, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Image, ImageBackground, StyleSheet, Switch, Button, Alert,TouchableOpacity } from 'react-native';
 
 import colors from '../config/colors'
 import News from '../components/News'
 import Topics from '../components/Topics'
 
-//menu screen done by Adrian
 function MenuScreen(props) {
+    const [switchElectionEnabled, switchElection] = React.useState(false);
+    const [switchCovidEnabled, switchCovid] = React.useState(false);
+    const [switchBLMEnabled, switchBLM] = React.useState(false);
+    const [switchGunControlEnabled, switchGunControl] = React.useState(false);
+    const [switchSameSexEnabled, switchSameSex] = React.useState(false);
+    const [message, setMessage] = useState("Searching for partner");
     return (
         <ImageBackground 
         blurRadius = {3}
@@ -16,91 +21,124 @@ function MenuScreen(props) {
         <View style = {styles.logo}>
             <Image style = {styles.image} resizeMode="contain" source ={require('../assets/LogoGray.png')}/>
         </View>
-
-        <View style = {styles.profile}>
+        <TouchableOpacity onPress={()=> Alert.alert("Signout", "",[
+            {text: "Cancel"},
+            {text: "Confirm"},
+        ])} style = {styles.profile}>
             <Image style = {styles.image} source ={require('../assets/profileAdrian.jpeg')}/>
-        </View>
+        </TouchableOpacity>
         <View style ={styles.newscard}>
+        <Text style ={styles.title}>Recent News</Text>
+
+            <News
+                title="Trump pardons Michael Flynn"
+                publisher="LA Times"
+                date="11/27/20"
+            />
+            <News
+                title="Top Black Friday Deals"
+                publisher="BuzzFeed"
+                date="11/27/20"
+            />
+            <News
+                title="Surprising Grammy Nominations"
+                publisher="LA times"
+                date="11/24/20"
+            />
+            <News
+                title="China and India tensions rise"
+                publisher="CNN"
+                date="11/24/20"
+            />
             <News
                 title="Gov. Newsom Quarantining"
                 publisher="NPR"
                 date="11/23/20"
-                subTitle="California Gov. Gavin Newsom and his family are quarantining after coming in contact with a Patrol Officer who tested positive for COVID-19"
             />
             <News
                 title="Trump refuses to Concede"
                 publisher="FOX News"
                 date="11/23/20"
-                subTitle="GSA Chief warns that Pres. elect Biden will continue presidential transition reguardless of Pres. Trump's actions"
             />
             <News
                 title="Ken Jennings new Jeopardy Host"
                 publisher="The Hill"
                 date="11/23/20"
-                subTitle="Ken Jennings has been named interim 'Jeopardy!' host after death of Alex Trebek"
             />
             <News
                 title="Economic recession likely"
                 publisher="CNN"
                 date="11/20/20"
-                subTitle="With the pandemic intensifying, experts from JPMorgan warn the GDP will turn negative by early next year"
             />
             <News
                 title="Biden wins election"
                 publisher="AP News"
                 date="11/12/20"
-                subTitle="Ex-Vice president Joe Biden has been chosen to be the next president of the United States"
             />
+    
+        <Button title="Show more" 
+        color="#6e6969"
+        onPress={() => Alert.alert("Under construction")}
+        />
         </View>
         <View style ={styles.topicsCard}>
-            <Text style ={{fontSize:17}}>Popular Topics</Text>
+            <Text style ={styles.title}>Popular Topics</Text>
             <Text style={{fontWeight: 'bold',textDecorationLine: 'underline',marginTop:5}}>This Week</Text>
             <Topics 
-                topic = "2020 Election"
-                number = "4.3K"
-            />
-            <Topics 
-                topic = "COVID-19 Vaccine"
-                number = "3.9K"
-            />
-            <Topics 
-                topic = "BLM Movement"
-                number = "3.7K"
-            />  
-            <Topics 
-                topic = "Gun Control"
-                number = "2.8K"
-            />
-            <Topics 
-                topic = "Raising Minimum Wage"
-                number = "2.7K"
-            />
-            <Topics 
-                topic = "Immigration Reform"
-                number = "2.2K"
-            />
-            <Topics 
-                topic = "Abortion"
-                number = "2.1K"
-            />
-            <Topics 
-                topic = "Foreign Aid"
-                number = "1.7K"
-            />
-        <Text style ={{color:'#6e6969', fontSize:15,marginVertical:10}}>Show More</Text>                                                   
-        </View>
-        <View style = {styles.navBottom}>
-            <Text>Menu Bar items would go here</Text>
+                    topic = "2020 Election"
+                    number = "4.3K"
+                /> 
+                <Switch
+                    value={switchElectionEnabled}
+                    style={styles.switchStyle}
+                    onValueChange={(value) => switchElection(value)}
+                /> 
+        <Topics 
+                    topic = "COVID-19 Vaccine"
+                    number = "4.2K"
+                /> 
+                <Switch
+                    value={switchCovidEnabled}
+                    style={styles.switchStyle}
+                    onValueChange={(value) => switchCovid(value)}
+                /> 
+        <Topics 
+                    topic = "BLM Movement"
+                    number = "4.0K"
+                /> 
+                <Switch
+                    value={switchBLMEnabled}
+                    style={styles.switchStyle}
+                    onValueChange={(value) => switchBLM(value)}
+                /> 
+        <Topics 
+                    topic = "Gun Control"
+                    number = "2.8K"
+                /> 
+                <Switch
+                    value={switchGunControlEnabled}
+                    style={styles.switchStyle}
+                    onValueChange={(value) => switchGunControl(value)}
+                /> 
+        <Topics 
+                    topic = "Same Sex Marriage"
+                    number = "2.5K"
+                /> 
+                <Switch
+                    value={switchSameSexEnabled}
+                    style={styles.switchStyle}
+                    onValueChange={(value) => switchSameSex(value)}
+                /> 
+        <Button title="Search" color="grey"/>
         </View>
         <View style = {styles.friendsListCard}>
-        <Text style ={{fontSize:17, marginBottom:10}}>Friends Online (3/14)</Text>
+        <Text style ={styles.title}>Friends Online (3/14)</Text>
             <View>
-                <Text style = {styles.FriendsList}>Cristian Ornelas</Text>
+                <Text style = {styles.FriendsList}>Adrian Leung</Text>
                 <Text style = {styles.FriendsList}>Jason Kalili</Text>
                 <Text style = {styles.FriendsList}>Jeffrey Shoji</Text>
             </View>
         </View>
-        
         </ImageBackground>
     );
 }
@@ -110,13 +148,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent:"flex-end",
     },
-    
     logo: {
         position: 'absolute',
-        left: 20,
-        top: '2%',
+        left: '5%',
+        top: '5%',
     },
-
     FriendsList:{
         fontSize:15,
         marginVertical:7,
@@ -126,34 +162,32 @@ const styles = StyleSheet.create({
         borderRadius: 15,
         backgroundColor: colors.offWhite,
         marginLeft: '5%',
-        height: '75%',
+        height: '80%',
         width: '42%',
-        top:'12%',
+        top:'15%',
         alignItems:"center",
     },
-
     topicsCard: {
         position: "absolute",
         borderRadius: 15,
         backgroundColor: colors.offWhite,
         marginLeft: 20,
-        height: '53%',
+        height: '57%',
         width: '43%',
-        top:'12%',
+        top:'15%',
         right:'5%',
         alignItems: "center",
         marginBottom:20,
         fontSize:20,
     },
-
     friendsListCard: {
         position: "absolute",
         borderRadius: 15,
         backgroundColor: colors.offWhite,
         marginLeft: 20,
-        height: '21%',
+        height: '22%',
         width: '43%',
-        top:'66%',
+        top:'73%',
         right:'5%',
         alignItems: "center",
     },
@@ -169,12 +203,24 @@ const styles = StyleSheet.create({
         width: 60,
         height: 60,
         borderRadius: 30,
+        
     },
-    
+    topicSelect:{
+        flexDirection:"row",
+    },
+    switchStyle:{
+        transform: [{ scaleX: .8 }, { scaleY: .8 }],
+        marginVertical:3,
+    },
+    title:{
+        top:10,
+        fontSize:20,
+        marginBottom:15
+    },
     profile:{
         position: 'absolute',
-        right: 20,
-        top: '2%',
+        right: '5%',
+        top: '5%',
     }
 })
 export default MenuScreen;
