1b34add26087a22b2423cb6fcdaea1a58e3f1ec8
cristianornelas2 <46763991+cristianornelas2@users.noreply.github.com>
2020-11-27 19:41:43 -0800

    Update CompassTest.js

--- app/screens/CompassTest.js
+++ app/screens/CompassTest.js
@@ -1,54 +1,126 @@
-import React from "react";
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
-import AppButton from "../components/AppButton";
+import React from 'react';
+import {View, ImageBackground, StyleSheet, Text, Image} from 'react-native';
+import colors from "../config/colors";
+import AppButton from '../components/AppButton'; 
+import Prompt from '../components/Prompt';
+import CompassButton from "../components/CompassButton";
+import {AntDesign} from '@expo/vector-icons';
 
-//compass test done by Cristian
 function CompassTest(props) {
-  return (
-    <ImageBackground
-      blurRadius={5}
-      style={styles.background}
-      source={require("../assets/compassBackground.jpg")}
-    >
-      <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/LogoGray.png")} />
-        <Text style={styles.tagline}>Politcal Compass Test</Text>
-      </View>
+    return (
+        <ImageBackground
+        blurRadius ={7}
+        style = {styles.background}        
+        source={require("../assets/welcome.jpg")} 
+        >
+        <View style={styles.logoIcon}>
+            <Image style = {styles.image} resizeMode = "contain" source={require("../assets/logo.png")} />
+        </View>
 
-      <View style={styles.buttonsContainer}>
-        <AppButton title="Begin Test" />
-        <AppButton title="Go Back " />
-      </View>
-    </ImageBackground>
-  );
-}
+        <View style={styles.profileIcon}>
+            <Image style = {styles.image} source={require('../assets/cristianprofile.jpg')}/>
+        </View>
 
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  buttonsContainer: {
-    padding: 5,
-    width: "65%",
-    justifyContent: "center",
-  },
-  logo: {
-    width: 200,
-    height: 130,
-  },
-  logoContainer: {
-    position: "absolute",
-    top: 70,
-    alignItems: "center",
-    fontSize: 70,
-  },
-  tagline: {
-    fontSize: 25,
-    fontWeight: "600",
-    paddingVertical: 20,
-  },
-});
+        <View style ={styles.forum}>
+            <Prompt
+                title="Question 1:"
+                subTitle="If economic globalisation is inevitable, it should primarily serve humanity rather than the interests of trans-national corporations."
+            />
+            <Prompt
+                title="Question 2:"
+                subTitle="I’d always support my country, whether it was right or wrong."
+            />
+            <Prompt
+                title="Question 3:"
+                subTitle="No one chooses their country of birth, so it’s foolish to be proud of it."
+            />
+            <Prompt
+                title="Question 4:"
+                subTitle="Our race has many superior qualities, compared with other races."
+            />
+        </View>
+        <View style={styles.submitContainer}>
+            
+        <CompassButton title ="Submit" />
+        </View>
+
+
+        {/* <View style={styles.faceIcon}></View> */}
+                    {/* <Image style={styles.logo} source={require("../assets/logo.png")} /> */}
+               {/* <View style={styles.whitebackground}> */}
+                {/* <View style={styles.textContainer}> */}
+                    {/* <Text style={styles.question}>QUESTION 1:</Text>
+                    <Text style={styles.question}>If economic globalisation is inevitable, it should primarily serve humanity rather than the interests of trans-national corporations.</Text>
+                <View style={styles.buttonsContainer}>   
+                <AppButton title ="Agree" />
+                <AppButton title = "Neutral"/>
+                <AppButton title = "Disagree"/>
+                </View> */}
+                {/* </View> */}
+        
+                {/* <View style={styles.textContainer}> */}
+                    {/* <Text style={styles.question}>QUESTION 2:</Text>
+                    <Text style={styles.question}>hello</Text>
+                <View style={styles.buttonsContainer}>
+                <AppButton title ="Agree" />
+                <AppButton title = "Neutral"/>
+                <AppButton title = "Disagree"/>
+                </View>     */}
+                {/* </View> */}
+
+
+                {/* </View> */}
+                
+                </ImageBackground>
+                 
+            );
+        }
+        
+        const styles = StyleSheet.create({
+            background: {
+                flex: 1,
+                justifyContent: "flex-end",
+            },
+            buttonsContainer: {
+                padding: 10,
+                width: "55%",
+                justifyContent: 'center',
+                flexDirection: "row",
+            },
+            forum: {
+                position: "absolute",
+                borderWidth: 10,
+                borderRadius: 35,
+                borderBottomWidth: 64,
+                borderColor: colors.white,
+                backgroundColor: colors.black,
+                margin: 10,
+                height: "85%",
+                width: "95%",
+                top: "13%",
+            },
+            image:{
+                width: 60,
+                height: 60,
+                borderRadius: 30,
+            },
+            logoIcon:{
+                position: "absolute",
+                top: "5%",
+                left: 20,
+            },
+            profileIcon:{
+                position: "absolute",
+                top: "5%",
+                right: 20,
+            },
+            submitContainer: {
+                    margin: "5%",
+                    justifyContent: 'center',
+                    width: "90%",
+                    
+                    
+            },
+        })
 
 export default CompassTest;
