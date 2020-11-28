0ae92e51dab062e4f95f424bf48a8c874f02a4fb
cristianornelas2 <46763991+cristianornelas2@users.noreply.github.com>
2020-11-27 20:02:57 -0800

    Update CompassButton.js

--- app/components/CompassButton.js
+++ app/components/CompassButton.js
@@ -1,71 +1,46 @@
-import React, {Component} from "react";
+import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import colors from "../config/colors";
 
 
-function CompassButton ({title, onPress, color}) {
-state ={
-    toggle: true
-}
-
-_onPress() ; {
-    const newState = !this.state.toggle;
-    this.setState({toggle:newState})
-}
-
-render () ; {
-    const {toggle} = this.state;
-    const buttonBg = toggle?"colors.white":"dodgerblue";
-    const textColor = toggle?"colors.black":"colors.white";
+function CompassButton({title, onPress, color}) {
+    
+    
+    
+    
+    
+    
     return (
 
-        <View>
-        <TouchableOpacity 
-        onPress={()=>this._onPress} 
-        style={{height: 35,
-            backgroundColor: buttonBg,
-            borderRadius: 11,
-            borderColor: colors.primary,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 5,
-            marginVertical: 5,
-            marginHorizontal: 25,
-            flexDirection: "row",}}>
-            
-            
-            <Text style={{color:textColor,
-                 fontSize: 13,}}>{title} </Text>
+  
 
-
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.text}> {title} </Text>
         </TouchableOpacity>
 
-
-        </View>
-     );
-    }
+    );
 }
 
 const styles = StyleSheet.create({
-    // button: {
-    //     //width: "22%",
-    //     height: 35,
-    //     backgroundColor: buttonBg,
-    //     borderRadius: 11,
-    //     borderColor: colors.primary,
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     borderWidth: 5,
-    //     marginVertical: 5,
-    //     marginHorizontal: 25,
-    //     flexDirection: "row",
-    // },
-    // text: {
-    //     color: colors.black,
-    //     fontSize: 13,
-    //     //fontWeight: 'bold',
+    button: {
+        //width: "22%",
+        height: 35,
+        backgroundColor: colors.white,
+        borderRadius: 11,
+        borderColor: colors.primary,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 5,
+        marginVertical: 5,
+        marginHorizontal: 25,
+        flexDirection: "row",
+    },
+    text: {
+        color: colors.black,
+        fontSize: 13,
+        //fontWeight: 'bold',
 
-    // },
+    }
 })
 
 export default CompassButton;
